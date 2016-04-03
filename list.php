c8683b3767f4928015ac773b6aa7d8358ceef6e3
Pedro Bucho <pedro.bucho@gmail.com>
2016-04-03 16:41:30 +0100

    Changed table to Datatable

--- list.php
+++ list.php
@@ -9,18 +9,20 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
 			integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
 			crossorigin="anonymous">
+		<!-- DATATABLES -->
+		<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.11/css/jquery.dataTables.css">
 	</head>
 	<body>
 		<div class="container">
 			<h1>Redirector URL list</h1>
 			<p>Short URLs to be used in the format <code>r.bucho.pt/shorturl</code></p>
-			<table class="table table-hover">
-				<tr>
+			<table class="table table-hover" id="link_table">
+				<thead>
 					<th>Short URL</th>
 					<th>Long URL</th>
 					<th>Date added</th>
 					<th>Views</th>
-				</tr>
+				</thead>
 				<?php
 					$sql = "SELECT * FROM translation";
 					$conn = getConnection();
@@ -39,10 +41,22 @@
 					}
 				?>
 			</table>
+			<!-- JQUERY -->
+			<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.js">
+			</script>
 			<!-- BOOTSTRAP -->
 			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
 				integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
 				crossorigin="anonymous">
+			</script>
+			<!-- DATATABLES -->
+			<script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.11/js/jquery.dataTables.js">
+			</script>
+	
+			<script type="text/javascript">
+				$(document).ready(function() {
+					$("#link_table").DataTable();
+				});
 			</script>
 		</div>
 	</body>
