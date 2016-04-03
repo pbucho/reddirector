fa0cda6048ab4a6ff2356c78f560ab9dd0d2ca3d
Pedro Bucho <pedro.bucho@gmail.com>
2016-04-03 12:53:48 +0100

    Added Bootsrtap to list.php

--- list.php
+++ list.php
@@ -4,32 +4,44 @@
 	<!-- pbucho, 03-04-2016 -->
 	<head>
 		<title>Redirector URL list</title>
+		<!-- BOOTSTRAP -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+			integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
+			crossorigin="anonymous">
 	</head>
 	<body>
-		<table border="1">
-			<tr>
-				<th>Short URL</th>
-				<th>Long URL</th>
-				<th>Date added</th>
-				<th>Views</th>
-			</tr>
-			<?php
-				$sql = "SELECT * FROM translation";
-				$conn = getConnection();
-				$result = $conn->query($sql);
-				$conn = null;
-				
-				$result = $result->fetchAll();
-				
-				foreach($result as $item){
-					echo "<tr>";
-					echo "<td>".$item['short_url']."</td>";
-					echo "<td><a href='".$item['long_url']."'>".$item['long_url']."</a></td>";
-					echo "<td>".$item['added']."</td>";
-					echo "<td>".$item['views']."</td>";
-					echo "</tr>";
-				}
-			?>
-		</table>
+		<div class="container">
+			<h1>Redirector URL list</h1>
+			<table class="table">
+				<tr>
+					<th>Short URL</th>
+					<th>Long URL</th>
+					<th>Date added</th>
+					<th>Views</th>
+				</tr>
+				<?php
+					$sql = "SELECT * FROM translation";
+					$conn = getConnection();
+					$result = $conn->query($sql);
+					$conn = null;
+					
+					$result = $result->fetchAll();
+					
+					foreach($result as $item){
+						echo "<tr>";
+						echo "<td>".$item['short_url']."</td>";
+						echo "<td><a href='".$item['long_url']."'>".$item['long_url']."</a></td>";
+						echo "<td>".$item['added']."</td>";
+						echo "<td>".$item['views']."</td>";
+						echo "</tr>";
+					}
+				?>
+			</table>
+			<!-- BOOTSTRAP -->
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+				integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+				crossorigin="anonymous">
+			</script>
+		</div>
 	</body>
 </html>
