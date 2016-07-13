f6254d923a57bcb119a4cd9a3a1a146450d55974
Pedro Bucho <pedro@bucho.pt>
2016-07-13 20:29:21 +0100

    Updating interface to interact with API

---
+++ js/cookie_getter.js
@@ -0,0 +1,19 @@
+function getToken() {
+  var allCookies = document.cookie;
+  var splitten = allCookies.split(";");
+  if(splitten.length < 1){
+    return null;
+  }
+  //for(cookie in splitten) {
+  for(var i = 0; i < splitten.length; i++){
+    //var thisCookie = cookie.split("=");
+    var thisCookie = splitten[i].split("=");
+    if(thisCookie.length < 1){
+      continue;
+    }
+    if(thisCookie[0] == "token"){
+      return thisCookie[1];
+    }
+  }
+  return null;
+}
